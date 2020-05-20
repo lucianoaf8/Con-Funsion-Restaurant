0e7a4ac8f399f200cb5f5ed98deddcd2bb22410a
Luciano <lucianoaf8@gmail.com>
2020-05-20 16:33:29 -0600

    Add npm scripts

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// Carousel
+$("#carouselButton").click(function () {
+    if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+        $("#mycarousel").carousel("pause");
+        $("#carouselButton").children("span").removeClass("fa-pause");
+        $("#carouselButton").children("span").addClass("fa-play");
+    } else if ($("#carouselButton").children("span").hasClass("fa-play")) {
+        $("#mycarousel").carousel("cycle");
+        $("#carouselButton").children("span").removeClass("fa-play");
+        $("#carouselButton").children("span").addClass("fa-pause");
+    }
+});
+// Reservation modal
+$(".reserveButton").click(function () {
+    $("#reservationModal").modal("show");
+});
+$(".closeReservation").click(function () {
+    $("#reservationModal").modal("hide");
+});
+// Login Modal
+$(".loginButton").click(function () {
+    $("#loginModal").modal("show");
+});
+$(".closeLogin").click(function () {
+    $("#loginModal").modal("hide");
+});
